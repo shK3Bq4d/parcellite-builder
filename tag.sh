4a3bb7da61345e4666691c048faab0d8919e4e0c
Marc Rumo <marc.rumo@kudelskisecurity.com>
2018-07-09 09:36:35 +0200

    docker tag names can't have uppercase letters

--- tag.sh
+++ tag.sh
@@ -3,4 +3,4 @@
 
 DIR="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
 
-echo "$(basename "$(readlink -f "$DIR/..")")/$(basename "$DIR")"
+echo "$(basename "$(readlink -f "$DIR/..")")/$(basename "$DIR")" | tr '[:upper:]' '[:lower:]'
